1478af49e4dd0455460803f178e2e14649f41bb8
Matthew Planchant <matthew.planchant@digital.hmrc.gov.uk>
2017-03-29 12:35:00 +0100

    Adds mapper stuff.

---
+++ src/test/java/com/logiccache/hotel/util/MapperUtilTest.java
@@ -0,0 +1,33 @@
+package com.logiccache.hotel.util;
+
+import com.logiccache.hotel.api.model.GetBookingsResponse;
+import com.logiccache.hotel.domain.Booking;
+import org.dozer.DozerBeanMapper;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class MapperUtilTest {
+
+    @Autowired
+    private DozerBeanMapper mapper;
+
+    @Ignore
+    @Test
+    public void testAssembleSomeObject() throws Exception {
+        Booking source = Booking.builder().roomId("room").customerId("customer").fromDate(LocalDate.of(2010, Month.AUGUST, 21)).toDate(LocalDate.of(2010, Month.AUGUST, 22)).build();
+        GetBookingsResponse destination = mapper.map(source, GetBookingsResponse.class);
+        Booking mappedSource = mapper.map(destination, Booking.class);
+        assertEquals("fields not mapped correctly", source, mappedSource);
+    }
+}
